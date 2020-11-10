e1758804bc41b0e6118fe7625176aa51bcf50495
Adhi prasetia <adhi.prasetia@protonmail.com>
2020-11-10 11:47:27 +0700

    - Removed vpc_subnet_public - Added google_compute_image data - Added google_compute_firewall to allow SSH from public network (Internet)

--- google-free-tier/main.tf
+++ google-free-tier/main.tf
@@ -6,6 +6,7 @@ provider "google" {
 
 data "google_compute_zones" "available" {
   project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc" {
@@ -19,18 +20,11 @@ resource "google_compute_router" "vpc_router" {
   network = google_compute_network.vpc.self_link
 }
 
-resource "google_compute_subnetwork" "vpc_subnet_public" {
-  name                     = "${var.project_id}-subnet-public"
-  network                  = google_compute_network.vpc.self_link
-  private_ip_google_access = true
-  ip_cidr_range            = "10.0.0.0/16"
-}
-
 resource "google_compute_subnetwork" "vpc_subnet_private" {
   name                     = "${var.project_id}-subnet-private"
   network                  = google_compute_network.vpc.self_link
   private_ip_google_access = true
-  ip_cidr_range            = "10.1.0.0/16"
+  ip_cidr_range            = var.private_subnetwork_cidr_range
 }
 
 resource "google_compute_router_nat" "vpc_nat" {
@@ -45,18 +39,43 @@ resource "google_compute_router_nat" "vpc_nat" {
   }
 }
 
+data "google_compute_image" "vm_image" {
+  project = "centos-cloud"
+  family  = "centos-8"
+}
+
 resource "google_compute_instance" "vm_centos" {
   name         = "${var.project_id}-vm-centos"
   zone         = data.google_compute_zones.available.names[0]
   machine_type = "f1-micro"
+  tags         = ["ssh"]
 
   boot_disk {
     initialize_params {
-      image = "centos-8-v20201014"
+      image = data.google_compute_image.vm_image.self_link
     }
   }
 
   network_interface {
-    network = google_compute_subnetwork.vpc_subnet_private.self_link
+    network    = google_compute_network.vpc.self_link
+    subnetwork = google_compute_subnetwork.vpc_subnet_private.self_link
+  }
+
+  scheduling {
+    automatic_restart = false
+    preemptible       = true
+  }
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name          = "${var.project_id}-fw-ssh"
+  network       = google_compute_network.vpc.name
+  direction     = "INGRESS"
+  source_ranges = var.public_network_cidr_source_ranges
+  target_tags   = ["ssh"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
 }
