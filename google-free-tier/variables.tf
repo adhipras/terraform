f69e663e01c8764e66643cb3ce74440e5542c49c
Adhi prasetia <adhi.prasetia@protonmail.com>
2020-11-10 11:44:39 +0700

    Added default public_network_cidr_source_ranges and private_subnetwork_range.

--- google-free-tier/variables.tf
+++ google-free-tier/variables.tf
@@ -21,3 +21,15 @@ variable "zone" {
   type        = string
   default     = "us-central1-a"
 }
+
+variable "public_network_cidr_source_ranges" {
+  description = "The IP address range of the public network source ranges in CIDR notation."
+  type        = list
+  default     = ["0.0.0.0/0"]
+}
+
+variable "private_subnetwork_cidr_range" {
+  description = "The IP address range of the private subnetwork in CIDR notation."
+  type        = string
+  default     = "10.0.0.0/16"
+}
